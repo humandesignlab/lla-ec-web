4a9fc6fb20f158723e2a4eb26abc7c186f206384
Humberto Mangino <hmangino@gmail.com>
2020-02-13 17:20:07 -0600

    add button

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
+			<Button
+				text="Next"
+				textColor="#161616"
+				backgroundColor="#d3d3d3"
+        onPress={() =>
+          console.log("Pressed button!")
+        }
+				/>
     </View>
   );
 }
